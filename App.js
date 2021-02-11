daafdcb20c543484fa2c3438d1679f4b3809d927
Sadat Bin Sharfuddin <sadat.bs@gmail.com>
2021-02-12 00:56:16 +0600

    not working

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {getNews} from './src/components/news';
+import { FlatList } from 'react-native';
+import {Article} from './src/components/article';
 
 export default function App() {
+ //let news = getNews();
+ const [articles, setArticles] = useState([]);
+ const [refresh, setRefresh] = useState(true);
+
+ useEffect(() => {
+   fetchNews();
+ }, [refresh])
+
+ const fetchNews = async ()=>{
+
+  const response = await getNews().then((response)=>{
+    setArticles(response);
+    setRefresh(false);
+    console.log("sadat                 is ", articles);
+  });
+  
+ }
+
+const handleRefresh = () => {
+  setRefresh(true);
+  fetchNews();
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View>
+      <Text>sadat</Text>
+    <FlatList
+    data={articles}
+    renderItem={({ item }) => <Article article={item} />}
+    keyExtractor={item => item.url}
+    refreshing={refresh}
+    onRefresh={handleRefresh}
+  />
+  </View>
   );
 }
 
